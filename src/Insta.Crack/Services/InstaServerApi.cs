fd81fc7bb746f0340c812d83d5cd249d4e6565b8
lera ryabenko <valeria.ryabenko@gmail.com>
2016-04-16 18:30:56 +0300

    Add mock servr api. Page select

---
+++ src/Insta.Crack/Services/InstaServerApi.cs
@@ -0,0 +1,14 @@
+﻿namespace Insta.Crack.Services
+{
+	using System.Net.Http;
+
+	public class InstaServerApi
+	{
+		public void GetMyFeed(string userName)
+		{
+			using (var httpClient = new HttpClient())
+			{
+			}
+		}
+	}
+}
